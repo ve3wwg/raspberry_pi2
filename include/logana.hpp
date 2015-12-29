03e502f74d5431716585d8e01a7ba90d01722f25
Warren Gay <ve3wwg@gmail.com>
2015-12-28 19:20:46 -0500

    First cut at Linux 4.x

--- include/logana.hpp
+++ include/logana.hpp
@@ -31,12 +31,16 @@ class LogicAnalyzer {
 
     std::vector<s_block*> dma_blocks;
 
+    std::vector<uint32_t>  sg_list;    // Scatter/Gather list
+
     int                 fd;         // /dev/rpidma
     s_rpidma_ioctl      dalloc;     // DMA Allocation
     std::string         errmsg;     // Error message
     DmaMem              dmamem;     // DMA Memory
     DMA                 dma;        // DMA registers + CB
     GPIO                gpio;       // GPIO access
+
+    bool                rpidma4x;   // Using Linux kernel 4.x+ module
 
 protected:
     bool alloc_dma();
@@ -53,16 +57,10 @@ public:
     uint32_t get_gplev0();      // Return phys addr for GPLEV0
 
     bool alloc_blocks(unsigned blocks);
-    DMA::CB& get_cb();          // Get first DMA cb
-    void propagate();           // Propogate first DMA cb
 
-    void dump_cb();             // Dump control blocks to stdout (debugging)
+    bool start(unsigned long src_addr);
+    int is_completed();		// 1==completed, 0==incomplete or < 0 is error
 
-    bool start();               // Allocate and run
-    bool abort(DMA::s_DMA_CS *status); // Abort DMA in progress (and release)
-    bool end();                 // Return true if completed
-
-    unsigned get_interrupts();  // Return # of interrupts
     inline size_t get_blocks() { return dma_blocks.size(); }
 
     uint32_t *get_samples(unsigned blockx,size_t *n_samples);
